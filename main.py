985055afe01434ca77c401fd7c2c4d207ccc5cc8
Youta <0x59.xxx@gmail.com>
2025-02-27 12:37:18 +0900

    Change: change repository name

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import os
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import requests
+import urllib.parse
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+SPOTIFY_REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/spotify/search")
+def search track(query)
